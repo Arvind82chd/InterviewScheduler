bf137386f765edc47506e87fd14a5467fafd5b16
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-19 21:58:58 +0000

    added interviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem (props) {
+
+  const interviewerClass = classNames("interviewer__item", {"interviewer__item--selected": props.selected});
+
+
+  return (
+  <li onClick={() => setInterviewer(props.id)} className="interviewers__item">
+    <img
+      className="interviewers__item-image"
+      src="https://i.imgur.com/LpaY82x.png"
+      alt="Sylvia Palmer"
+    />
+    Sylvia Palmer
+  </li>
+  );
+}
