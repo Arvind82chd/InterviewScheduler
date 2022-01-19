b265294e7f2a750e1dd6fb9909d2b3b128b9fad2
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-19 18:18:46 +0000

    added the dynamic css to DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
+
+  const formatSpots = (spots) => {
+    if (spots === 1) {
+      return `${spots} spot ` 
+      
+    }
+    if (spots > 1) {
+      return `${spots} spots `
+    }
+    return `no spots `
+  }
   
+  const dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0});
+  
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} >
       <h2 className="test--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}remaining</h3>
 
     </li>
   );
