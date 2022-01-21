4ca23fbb17162388c53c6cbc636804639804d894
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-21 01:04:55 +0000

    updated the application.js with mock data and iterating over the data

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import "./Appointment";
+import Appointment from "./Appointment";
 
 
 const interviewers = [
@@ -30,6 +32,45 @@ const days = [
   },
 ];
 
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
@@ -61,7 +102,10 @@ export default function Application(props) {
       />
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
-      <section className="schedule">
+      <section className="schedule" > {appointments.map((appointment) => <Appointment 
+      key={appointment.id}
+      {...appointment}
+      />)}
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,11 +4,20 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 
+
+
 export default function Appointment (props) {
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {(props.interview) ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty /> }
+      <Header 
+      time={props.time}
+      />
+      {(props.interview) ? 
+      <Show 
+      student={props.interview.student} 
+      interviewer={props.interview.interviewer}
+      /> : 
+      <Empty /> }
       </article>
   );
 }
