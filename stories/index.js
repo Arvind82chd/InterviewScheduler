6b82034b5e059838cc354f6e2a537865fe263ca9
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-21 00:31:39 +0000

    updated the appointment index.js file with conditions for empty and show components

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -304,3 +304,16 @@ storiesOf("Appointment", module)
   onSave={action("onSave")}
   onCancel={action("onCancel")}
 />);
+
+// Appointment/Empty Stories:
+
+storiesOf("Appointment", module)
+.addParameters({
+  background: [{ name: "white", value: "#fff", default: true }]
+})
+.add("Appointment Empty", () => (
+  <Fragment>
+    <Appointment id={1} time="4pm" />
+    <Appointment time="5pm" />
+  </Fragment>
+))
