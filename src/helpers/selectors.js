32bf013c15652e8cbd7a812fd0918644a4d2dded
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-24 14:34:29 +0000

    correcting the getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,17 +2,19 @@
 
 export function getAppointmentsForDay (state, day) {
   //console.log(state)
-  return state.days.filter(d => d.name === day).map(d => d.appointments)
-  // return !filteredDays[0] ? [] : filteredDays[0].map((id) => state.appointments[id] )
+  const filteredDays = state.days.filter(d => d.name === day).map(d => d.appointments)
+  return !filteredDays[0] ? [] : filteredDays[0].map((id) => state.appointments[id] )
  
   // return selectedAppointments//filteredDays[0] || [];
 }
 
 export function getInterview(state, interview) {
-  if (!state.interviewer) {
+  if (!interview) {
     return null;
-  } return state.interview.filter((int => int.interview === interview).map(int => 
+  } //console.log("return values: ",state.interview.filter((int => int.interview === interview)))
+  return state.interviews.data(interview, state.interviewers.interview)
+  //filter(int => int.interview === interview).find(int => 
     //console.log(int.interview)
-     int.interview
-    )) 
+    //  int.interview
+    // )
 }
