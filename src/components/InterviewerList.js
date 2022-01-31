55f26d230bcec15adff4cd42c89f4221e5375ca2
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-31 21:38:22 +0000

    Added cypress

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,35 +3,21 @@ import "./InterviewerList.scss";
 import InterviewerListItem from './InterviewerListItem';
 import PropTypes from 'prop-types';
 
-// class Greeting extends React.Component {
-//   render() {
-//     return (
-//       <h1>Hello, {this.props.name}</h1>
-//     );
-//   }
-// }
-
-// Greeting.propTypes = {
-//   name: PropTypes.string
-// };
 
 function InterviewerList (props) {
 
-  //const {interviewers, setInterviewer} = props;
 
   const {interviewers, value, onChange} = props;
-  // const interviewerData = interviewers.map((interviewer) => {return (<InterviewerListItem {...interviewer}} selected={interviewer.id === props.interviewer} setInterviewer={setInterviewer}/>
-  // )});
+
   const interviewerData = interviewers.map((interviewer) => 
   {return (
   <InterviewerListItem 
     key={interviewer.id} 
     name={interviewer.name} 
     avatar={interviewer.avatar} 
-    // selected={interviewer.id === props.interviewer}
     selected={interviewer.id === value} 
     setInterviewer={() => onChange(interviewer.id)}
-    // setInterviewer={(event) => props.setInterviewer(interviewer.id)}
+  
   />
   )});
 
@@ -45,24 +31,6 @@ function InterviewerList (props) {
   );
 }
 
-// <script>
-//   function displayInterviewer() {
-//     const currentInterviewer = document.querySelector("#interviewer-list").value;
-//     document.querySelector("#interviewer-output").innerHTML = `The currently selected interviewer is: <strong>${currentInterviewer}</strong`;
-//   }
-// </script>
-
-// <p>Select an interviewer from the list.</p>
-
-// <select id="interviewer-list" onchange="displayInterviewer()">
-//   <option value="Sylvia Palmer">Sylvia Palmer</option>
-//   <option value="Tori Malcolm">Tori Malcolm</option>
-//   <option value="Mildred Nazir">Mildred Nazir</option>
-//   <option value="Cohana Roy">Cohana Roy</option>
-//   <option value="Sven Jones">Sven Jones</option>
-// </select>
-
-// <p id="interviewer-output">The currently selected interviewer is:</p>
 
 InterviewerList.propTypes = {
   interviewers: PropTypes.array.isRequired
